f35dbe4acc842a11890377af19e15e71ff018f4a
mach182 <manuel.colinh@gmail.com>
2015-02-20 01:08:43 -0600

    Codigo hasta primer while, problemas para hacer el alert despues de ese while

--- potencia.js
+++ potencia.js
@@ -1,9 +1,40 @@
 //Programa para elevar un numero n a una potencia p//
 var n = 0;
 var p = 0;
-var i = n;
+var i = 0;
+
 
 n = prompt("Escoge tu numero base");
 p = prompt("Escoge tu potencia");
-
+i = n
 alert("Tu numero base es " + n + "\nY tu potencia es " + p);
+
+if ( n == 0) 
+	{ 			//La potencia de 0 siempre va a ser 0
+	alert("Tu numero es " + n); 
+	
+	}
+	else if ( n == 1) 
+		{ 		//La potencia de 1 siempre va a ser 1
+		alert("Tu numero es " + n);
+		}
+
+		else if ( p == 0) 
+			{			 //Cualquier numero elevado a la potencia 0 es 1
+				n = 1;
+				alert("Tu numero es " + n);
+			}
+			else if (p < 0)
+			 {
+				
+				while (p != -1)
+				{
+					n = n * i;
+					alert(n);
+					p++;
+				}
+			
+
+			}
+
+				// else if (true) {}
