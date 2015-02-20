4bfec15a497359462df20cc50da2f83856942d98
mach182 <manuel.colinh@gmail.com>
2015-02-20 00:09:45 -0600

    Buena Practica

---
+++ potencia.js
@@ -0,0 +1,9 @@
+//Programa para elevar un numero n a una potencia p//
+var n = 0;
+var p = 0;
+var i = n;
+
+n = prompt("Escoge tu numero base");
+p = prompt("Escoge tu potencia");
+
+alert("Tu numero base es " + n + "\nY tu potencia es " + p);
